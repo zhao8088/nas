e0a868f007cb7b8a2dad57ee02d0f4483bec7ddf
zhao8088 <80254000@qq.com>
2026-03-10 13:33:33 +0800

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     const originalUrl = new URL(request.url);
-    const nasDomain = "nas.hyzx.top"; // 替换为实际M1域名
+    const nasDomain = "nas.hyzx.top:5000"; // 替换为实际M1域名
 
     // 构建目标URL，使用http://，不指定端口
     const nasUrl = `http://${nasDomain}${originalUrl.pathname}${originalUrl.search}`;
